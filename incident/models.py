db8a9c06b730de743f0f18474a54e4bfb385bbcb
teresa <teresajclark@gmail.com>
2020-10-22 19:21:50 +0100

    added classification ManyToManyField & owner ForeignKey

--- incident/models.py
+++ incident/models.py
@@ -12,6 +12,31 @@ class Incident(models.Model):
     link2 = models.CharField(max_length=300, null=True, blank=True)
     tag = models.CharField(max_length=100, null=True, blank=True)
     vetted = models.BooleanField(default=False)
+    owner = models.ForeignKey(
+        'jwt_auth.User',
+        related_name="filed_incident",
+        on_delete=models.CASCADE
+    )
+    countries = models.ManyToManyField(
+        'classification.Country',
+        related_name = "country_list",
+        blank=True
+    )
+    attack_classes = models.ManyToManyField(
+        'classification.Attack_Class',
+        related_name = 'attack_class_selection',
+        blank=True
+    ) 
+    attack_types = models.ManyToManyField(
+        'classification.Attack_Type',
+        related_name = 'attack_type_selection',
+        blank=True
+    )
+    target_classes = models.ManyToManyField(
+        'classification.target_class',
+        related_name = 'target_type_selection',
+        blank=True
+    )
 
     def __str__(self):
         return f'{self.date} - {self.description}'
