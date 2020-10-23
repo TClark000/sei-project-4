662d629a06de721cd21d46d5d4ec88450709911c
teresa <teresajclark@gmail.com>
2020-10-23 22:58:04 +0100

    added classfications (countries...) to populated incident serializer

--- incident/serializers/populated.py
+++ incident/serializers/populated.py
@@ -1,5 +1,10 @@
 from jwt_auth.serializers.nested import NestedUserSerializer
 from ..serializers.common import IncidentSerializer
+from classification.serializers.common import CountrySerializer, Attack_ClassSerializer, Attack_TypeSerializer, Target_ClassSerializer
 
 class PopulatedIncidentSerializer(IncidentSerializer):
     owner = NestedUserSerializer()
+    countries = CountrySerializer(many=True)
+    attack_classes = Attack_ClassSerializer(many=True)
+    attack_types = Attack_TypeSerializer(many=True)
+    target_classes = Target_ClassSerializer(many=True)
