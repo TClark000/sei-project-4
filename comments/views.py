e77d517335019255d7bbc93819ac6e97dd9ee650
teresa <teresajclark@gmail.com>
2020-10-23 16:12:48 +0100

    comment detail and list view post & delete

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from rest_framework.exceptions import NotFound, PermissionDenied
+
+from rest_framework.permissions import IsAuthenticated
+
+from .serializers.common import CommentSerializer
+from .models import Comment
+
+class CommentListView(APIView):
+
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        # request.data['owner'] = request.user.id
+        comment_to_create = CommentSerializer(data=request.data)
+        if comment_to_create.is_valid():
+            comment_to_create.save()
+            return Response(comment_to_create.data, status=status.HTTP_201_CREATED)
+        return Response(comment_to_create.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+class CommentDetailView(APIView):
+
+    permission_classes = (IsAuthenticated,)
+
+    def get_comment(self, pk):
+        try:
+            return Comment.objects.get(pk=pk)
+        except Comment.DoesNotExist:
+            raise NotFound()
+    
+    def is_comment_owner(self, comment, user):
+        if comment.owner.id != user.id:
+            raise PermissionDenied()
+
+    def delete(self, request, pk):
+        comment_to_delete = self.get_comment(pk=pk)
+        self.is_comment_owner(comment_to_delete, request.user)
+        comment_to_delete.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
