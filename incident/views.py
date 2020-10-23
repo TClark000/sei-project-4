fef18ad2fa57960fac6f932b958b15d747405c89
teresa <teresajclark@gmail.com>
2020-10-23 14:48:33 +0100

    added ListView post api

--- incident/views.py
+++ incident/views.py
@@ -11,3 +11,12 @@ class IncidentListView(APIView):
         incident_list = Incident.objects.all()
         serialized_incident_list = IncidentSerializer(incident_list, many=True)
         return Response(serialized_incident_list.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        request.data['owner'] = request.user.id
+        incident_to_create = IncidentSerializer(data=request.data)
+        if incident_to_create .is_valid():
+            incident_to_create.save()
+            return Response(incident_to_create.data, status=status.HTTP_201_CREATED)
+        return Response(incident_to_create.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+    
