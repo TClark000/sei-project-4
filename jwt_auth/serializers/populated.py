26d9d68aac19a2c6f9179925be7be1d3f6d9c830
teresa <teresajclark@gmail.com>
2020-10-24 10:00:08 +0100

    added populated incident serializer to profile api

--- jwt_auth/serializers/populated.py
+++ jwt_auth/serializers/populated.py
@@ -1,7 +1,8 @@
-from incident.serializers.common import IncidentSerializer
+# from incident.serializers.common import IncidentSerializer
+from incident.serializers.populated import PopulatedIncidentSerializer
 from comments.serializers.common import CommentSerializer
 from ..serializers.common import UserSerializer
 
 class PopulatedUserSerializer(UserSerializer):
-    filed_incident = IncidentSerializer(many=True)
+    filed_incident = PopulatedIncidentSerializer(many=True)
     posted_comments = CommentSerializer(many=True)
