8ea03e584c0f31999b2b31ec6459d72314957565
teresa <teresajclark@gmail.com>
2020-10-26 11:58:17 +0000

    api fetch incident data, register component test

--- frontend/src/components/auth/Register.js
+++ frontend/src/components/auth/Register.js
@@ -1,6 +1,24 @@
 import React from 'react'
 
+import { incidentData } from '../../lib/api'
+
 class Register extends React.Component {
+
+  state = {
+    formData: {
+      username: '',
+      email: '',
+      first_name: '',
+      last_name: '',
+      password: '',
+      passwordConfirmation: ''
+    }
+  }
+
+  async componentDidMount() {
+    const response = await incidentData()
+    console.log(response)
+  }
 
   render() {
 
--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,5 +1,3 @@
-
-
 // async componentDidMount() {
 //   try {
 //     const response = await fetch('/api/incidents')
@@ -9,3 +7,32 @@
 //     console.log(err)
 //   }
 // }
+
+export const incidentData = async () => {
+  try {
+    const response = await fetch('/api/incidents')
+    return await response.json()
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export const registerUser = async () => {
+  try {
+    const response = await fetch('/api/auth', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({ 'formData': 'formData' })
+    })
+    // .then(response => {
+    //   return response.json()
+    // })
+    console.log(response.json())
+    return await response.json()
+    
+  } catch (err){
+    console.log(err)
+  }
+}
