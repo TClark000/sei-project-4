08ddf873b3b71bd42ef4670c47eac3311facf39c
teresa <teresajclark@gmail.com>
2020-11-04 16:32:27 +0000

    deployment

--- project/settings.py
+++ project/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os
 from pathlib import Path
-import django_heroku
 import django_on_heroku
 from dotenv import load_dotenv
 import dj_database_url
@@ -161,5 +160,4 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'frontend', "build", "static"),
 )
 
-django_heroku.settings(locals())
 django_on_heroku.settings(locals())
