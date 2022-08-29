badc94add3d972844fbe0b7a75474b047ee8ce17
tclark <teresajclark@gmail.com>
2022-08-29 20:15:05 +0100

    settings

--- project/settings.py
+++ project/settings.py
@@ -156,7 +156,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = BASE_DIR / '/staticfiles/'
 STATICFILES_DIRS = [
     # ...
     ("static", "frontend/build")
